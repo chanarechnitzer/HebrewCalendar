4e54e48eae41b4113f8cddbdbc3c3e56fa117374
chanarechnitzer <36851236+chanarechnitzer@users.noreply.github.com>
2025-05-18 22:55:31 +0300

    Added vite.config.ts.timestamp-1747598129998-949511ee4b017.mjs

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+   base: '/HebrewCalendar/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   }, 
-  base: '/HebrewCalendar/',
   build: {
     outDir: 'docs'
   }
