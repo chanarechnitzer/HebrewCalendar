e9b47691d9e038988e650c5c26b0e5438737941b
chanarechnitzer <36851236+chanarechnitzer@users.noreply.github.com>
2025-05-18 22:59:57 +0300

    Rebuild docs folder

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,12 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
    base: '/HebrewCalendar/',
+    build: {
+    outDir: 'docs'
+  },
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
-  }, 
-  build: {
-    outDir: 'docs'
-  }
+  } 
+ 
 });
